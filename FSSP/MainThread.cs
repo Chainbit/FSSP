07867ad2494e2aa7b2f9388208689d3f4ad4d6ef
Андрей Закатов <andreyzakatov@gmail.com>
2017-07-07 16:41:39 +0300

    MainThread

---
+++ FSSP/MainThread.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FSSP
+{
+    class MainThread
+    {
+        static List<Thread> allThreads = new List<Thread>();
+
+        public void CreateThreads(int i)
+        {
+            if (i < 1) return;
+
+
+        }
+    }
+}

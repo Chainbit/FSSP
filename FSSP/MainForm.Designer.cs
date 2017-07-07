500c9cf123bcd05eff0f2d3ecf1f71aa04203ea4
Rebel028 <cheburashka028@gmail.com>
2017-07-07 14:01:46 +0300

    form

--- FSSP/MainForm.Designer.cs
+++ FSSP/MainForm.Designer.cs
@@ -35,6 +35,11 @@
             this.setProxyPath = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
             this.setOutputPath = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.startButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -42,7 +47,7 @@
             this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI Light", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(31, 9);
+            this.label1.Location = new System.Drawing.Point(1, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(296, 37);
             this.label1.TabIndex = 0;
@@ -50,8 +55,9 @@
             // 
             // label2
             // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(38, 85);
+            this.label2.Location = new System.Drawing.Point(19, 85);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(157, 13);
             this.label2.TabIndex = 1;
@@ -59,54 +65,122 @@
             // 
             // setExcelPath
             // 
-            this.setExcelPath.Location = new System.Drawing.Point(201, 80);
+            this.setExcelPath.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.setExcelPath.Location = new System.Drawing.Point(182, 80);
             this.setExcelPath.Name = "setExcelPath";
             this.setExcelPath.Size = new System.Drawing.Size(75, 23);
             this.setExcelPath.TabIndex = 2;
             this.setExcelPath.Text = "Обзор...";
             this.setExcelPath.UseVisualStyleBackColor = true;
+            this.setExcelPath.Click += new System.EventHandler(this.setExcelPath_Click);
             // 
             // label3
             // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(38, 126);
+            this.label3.Location = new System.Drawing.Point(19, 126);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(87, 13);
             this.label3.TabIndex = 3;
             this.label3.Text = "Файл с прокси:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // setProxyPath
             // 
-            this.setProxyPath.Location = new System.Drawing.Point(201, 121);
+            this.setProxyPath.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.setProxyPath.Location = new System.Drawing.Point(182, 121);
             this.setProxyPath.Name = "setProxyPath";
             this.setProxyPath.Size = new System.Drawing.Size(75, 23);
             this.setProxyPath.TabIndex = 4;
             this.setProxyPath.Text = "Обзор...";
             this.setProxyPath.UseVisualStyleBackColor = true;
+            this.setProxyPath.Click += new System.EventHandler(this.setProxyPath_Click);
             // 
             // label4
             // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(38, 166);
+            this.label4.Location = new System.Drawing.Point(19, 166);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(72, 13);
             this.label4.TabIndex = 5;
             this.label4.Text = "Сохранить в:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // setOutputPath
             // 
-            this.setOutputPath.Location = new System.Drawing.Point(201, 161);
+            this.setOutputPath.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.setOutputPath.Location = new System.Drawing.Point(182, 161);
             this.setOutputPath.Name = "setOutputPath";
             this.setOutputPath.Size = new System.Drawing.Size(75, 23);
             this.setOutputPath.TabIndex = 6;
             this.setOutputPath.Text = "Выбор...";
             this.setOutputPath.UseVisualStyleBackColor = true;
+            this.setOutputPath.Click += new System.EventHandler(this.setOutputPath_Click);
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(19, 207);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(113, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Количество потоков:";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.numericUpDown1.Location = new System.Drawing.Point(182, 207);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            256,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(75, 20);
+            this.numericUpDown1.TabIndex = 8;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // startButton
+            // 
+            this.startButton.BackColor = System.Drawing.SystemColors.Control;
+            this.startButton.Location = new System.Drawing.Point(22, 252);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(110, 40);
+            this.startButton.TabIndex = 9;
+            this.startButton.Text = "Старт";
+            this.startButton.UseVisualStyleBackColor = false;
+            // 
+            // stopButton
+            // 
+            this.stopButton.Location = new System.Drawing.Point(147, 252);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(110, 40);
+            this.stopButton.TabIndex = 10;
+            this.stopButton.Text = "Стоп";
+            this.stopButton.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(355, 456);
+            this.ClientSize = new System.Drawing.Size(295, 457);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.setOutputPath);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.setProxyPath);
@@ -116,6 +190,7 @@
             this.Controls.Add(this.label1);
             this.Name = "MainForm";
             this.Text = "MainForm";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -130,5 +205,9 @@
         private System.Windows.Forms.Button setProxyPath;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button setOutputPath;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button stopButton;
     }
 }
